1f583a5ae56771d8878a6a2d6e543144fc0f1326
sachinkushwaha07 <55089298+sachinkushwaha07@users.noreply.github.com>
2020-02-21 17:35:12 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>project by Sachin and Abhinav</b>
+    </div>
+    <strong>profile <a href="https://www.facebook.com/campaign/landing.php?campaign_id=1653993517&extra_1=s%7Cc%7C318504235901%7Ce%7Cfacebook%27%7C&placement&creative=318504235901&keyword=facebook%27&partner_id=googlesem&extra_2=campaignid%3D1653993517%26adgroupid%3D63066387003%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D1t1%26target%3D%26targetid%3Dkwd-362360550869%26loc_physical_ms%3D1007768%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQiAtrnuBRDXARIsABiN-7Awg7My_xJI733SS1wRhOuWTf5Otih-QmbVCyK0xJWMO3oROXuse5gaAv61EALw_wcB">presenter's</a></strong>
+</footer>
